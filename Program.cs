d3df23e2cadc8c35506e56c27c3955df54027ff7
CesarDev <cuentas.oficial.cesardev@email.com>
2026-04-07 15:35:09 -0400

    Update Program.cs: agregar información del creador y documentos de punto de entrada

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MiniSistemaFacturacion
     static class Program
     {
         /// <summary>
-        /// Punto de entrada principal para la aplicación.
+        /// Punto de inicio principal para la aplicación.
         /// </summary>
         [STAThread]
         static void Main()
